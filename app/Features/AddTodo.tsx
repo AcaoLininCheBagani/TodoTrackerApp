915bcf7284af4860018429a2440d3d7a4ad3ad19
linin che bagani p. acao <lininchebagani.acao@fligno.com>
2025-10-29 23:47:02 +0800

    #0: added add todo

--- app/Features/AddTodo.tsx
+++ app/Features/AddTodo.tsx
@@ -1,34 +1,35 @@
 import { Plus, Mic } from 'lucide-react';
 import { AddTodoProps } from '../entities/todos';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Card } from '@/components/ui/card';
+export default function AddTodo({ newTodo, setNewTodo, addTodo }: AddTodoProps) {
+  return (
+    <Card className="p-6 mb-8">
+      <div className="flex gap-3">
+        <Input
+         className="h-10 px-4 text-lg"
+          type='text'
+          value={newTodo}
+          placeholder="Add a new task..."
+          onChange={(e) => setNewTodo(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && !e.repeat && addTodo()}
+        />
+        <Button
+          variant="outline" size="lg"
+          onClick={addTodo}
+        >
+          <Plus className="w-5 h-5" />
+          Add
+        </Button>
 
-export default function AddTodo({newTodo, setNewTodo, addTodo} : AddTodoProps){
-    return (
-        <div className="bg-white rounded-xl shadow-sm p-6 mb-8 border border-gray-100">
-          <div className="flex gap-3">
-            <input
-              type="text"
-              value={newTodo}
-              onChange={(e) => setNewTodo(e.target.value)}
-              placeholder="Add a new task..."
-              className="flex-1 px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              onKeyDown={(e) => e.key === 'Enter' && !e.repeat && addTodo()}
-            />
-            <button
-              onClick={addTodo}
-              className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-200 flex items-center gap-2 font-medium"
-            >
-              <Plus className="w-5 h-5" />
-              Add
-            </button>
-
-            <button
-              onClick={addTodo}
-              className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-200 flex items-center gap-2 font-medium"
-            >
-              <Mic className="w-5 h-5" />
-            </button>
-
-          </div>
-        </div>
-    )
+        <Button
+          variant="outline" size="lg"
+          onClick={addTodo}
+        >
+          <Mic className="w-5 h-5" />
+        </Button>
+      </div>
+    </Card>
+  )
 }
